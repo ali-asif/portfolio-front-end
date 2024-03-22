3d9e4f58b22e4e7b97e5f4472a98de1fa1b55783
ali-asif <ali16121999@gmail.com>
2024-03-22 21:16:12 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ["pbs.twimg.com", "i.ibb.co"]
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
